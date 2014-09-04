f766527e31c7b321661809647acc10cf05406c89
sarsees <reehl.sarahmarie@gmail.com>
2014-09-04 15:29:08 -0600

    Renamed the hw1 directory to hw

---
+++ hw/Assignment1/all_species_counts.sh
@@ -0,0 +1,7 @@
+for filename in data_*.txt
+do
+	echo $filename
+	python species_counts.py $filename |
+	sort -k 2 -n
+done
+
---
+++ hw/Assignment1/get_most_common_species.sh
@@ -0,0 +1,4 @@
+
+sort data_drycanyon_2013.txt -k 3 -nn | tail -1 sorted_counts.txt > most_common_species.txt
+curl -O http://www.programmingforbiologists.org/data/data_drycanyon_2013.txt
+ 
