3fe04e0ddaf0648c293266432c7c2b2ca37e70d6
sarsees <reehl.sarahmarie@gmail.com>
2014-09-21 09:28:24 -0600

    Plotting issues question

--- Help/plottingAttempt.py
+++ Help/plottingAttempt.py
@@ -1,13 +1,6 @@
-import pandas as pd
-import numpy as np
-
-def get_data_from_web(url):
-    data = pd.read_csv(url,delimiter='\t',names= ["Contintent","Status","Order","Family","Genus","Species","Log_Mass","Combined_Mass","Reference"])
-    return data
-
-MammalData = get_data_from_web('http://www.esapubs.org/archive/ecol/E084/094/MOMv3.3.txt')
-
+#3 Plotting
 import matplotlib.pyplot as plt
+#Separate into Extinct and Extant Mammals for AF
 AfricanMammals = pd.DataFrame(MammalData[MammalData['Contintent'] == 'AF'])
 ExtinctAfricanMammals = AfricanMammals[AfricanMammals['Status'] == 'extinct']
 ExtantAfricanMammals = AfricanMammals[AfricanMammals['Status'] == 'extant']
@@ -16,5 +9,7 @@ plt.subplot(1, 2, 1)
 plt.hist(ExtantAfricanMammals['Log_Mass'][ExtantAfricanMammals['Log_Mass'] > 0])#, len(ExtantAfricanMammals))
 plt.xlabel('Log Mass (g)', fontsize=20)
 plt.ylabel('Individuals', fontsize= 20)
+#Extant Mammals will plot without a hitch, but Extinct Mammals spit out several lines of error
 plt.hist(ExtinctAfricanMammals['Log_Mass'][ExtinctAfricanMammals['Log_Mass'] > 0])
 plt.show()
+#I noticed that the indexing is funny for ExtinctAfricanMammals, but I'm not sure why that matters
