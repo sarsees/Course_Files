4caebe7f1ae3b45982c5ffc06273b74be56caef0
sarsees <reehl.sarahmarie@gmail.com>
2014-09-24 14:25:51 -0600

    Dictionary working, but no way to access plot number

--- Assignment5/problem1.py
+++ Assignment5/problem1.py
@@ -5,24 +5,23 @@ def get_data_from_web(url):
     return data
 
 BirdData = get_data_from_web('http://www.esapubs.org/archive/ecol/E091/124/TGPP_pres.csv')
-#a function that calculates the Jaccard similiarity 
-#between a pair of sites when passed two sets as arguments
+#Make a dictionary with year as key and species by site as value
 speciesListByYear = dict()
 uniqueYears = set(BirdData['year'])
 uniquePlots = set(BirdData['plot'])
+plotSpeciesList = []
 for years in uniqueYears:
     yearData = BirdData[BirdData['year'] == years]
     for plots in uniquePlots:
         plotYearData = yearData[yearData['plot'] == plots]
-        plotYearSpeciesData = set(plotYearData['species'])
+        plotYearSpeciesData = set(plotYearData['spcode'])
         plotSpeciesList.append(plotYearSpeciesData)
-    speciesListByYear[year] = plotSpeciesList
+    speciesListByYear[years] = plotSpeciesList
 
-
-
-
-
+#a function that calculates the Jaccard similiarity 
+#between a pair of sites when passed two sets as arguments
 def calculate_Jaccard(speciesA,speciesB):
     J = speciesA.intersection(speciesB) / speciesA.union(speciesB)
     return J
 
+calculate_Jaccard(speciesListByYear[1998],speciesListByYear[1999])
