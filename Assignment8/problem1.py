b0d09167f2081bb7741a952289b3ed549146823f
sarsees <reehl.sarahmarie@gmail.com>
2014-10-27 17:52:50 -0600

    Define method "breeding_seasons" and tested on Perry

--- Assignment8/problem1.py
+++ Assignment8/problem1.py
@@ -5,5 +5,14 @@ class Platypus:
         
     def total_fecundity(self):
         return(sum(self.eggCount))
+    
+    def breeding_seasons(self):
+        count = 0
+        for eggs in self.eggCount:
+            if(eggs > 0):
+                count += 1
+        return(count)
+        
 perry = Platypus("perry", [3, 2, 4, 1, 2])
 print perry.total_fecundity()
+print perry.breeding_seasons()
