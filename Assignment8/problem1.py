d0f4df0004d5edbd8efdf3aef654ce188366161f
sarsees <reehl.sarahmarie@gmail.com>
2014-10-27 18:01:25 -0600

    All methods added and tested.

--- Assignment8/problem1.py
+++ Assignment8/problem1.py
@@ -12,7 +12,12 @@ class Platypus:
             if(eggs > 0):
                 count += 1
         return(count)
-        
+    
+    def lay_eggs(self,numEggs):
+        return self.eggCount.append(numEggs)
+    
 perry = Platypus("perry", [3, 2, 4, 1, 2])
 print perry.total_fecundity()
 print perry.breeding_seasons()
+perry.lay_eggs(2)
+print perry.total_fecundity()
