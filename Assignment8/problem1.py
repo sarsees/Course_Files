58335a3fe02e60b25626829518035776e8f5725e
sarsees <reehl.sarahmarie@gmail.com>
2014-10-29 15:42:26 -0600

    "Simulation complete and new undergrad data added and list comprehended."

--- Assignment8/problem1.py
+++ Assignment8/problem1.py
@@ -1,3 +1,4 @@
+import random
 class Platypus:
     def __init__(self,name,eggCount):
         self.name = name
@@ -16,8 +17,37 @@ class Platypus:
     def lay_eggs(self,numEggs):
         return self.eggCount.append(numEggs)
     
-perry = Platypus("perry", [3, 2, 4, 1, 2])
-print perry.total_fecundity()
-print perry.breeding_seasons()
-perry.lay_eggs(2)
-print perry.total_fecundity()
+    def platypus_details(self):
+        print "%s laid a total of %s eggs in %s successful breeding seasons." % (
+            self.name, self.total_fecundity(), self.breeding_seasons())
+
+    
+#Problem3    
+perry = Platypus("Perry",[0])                                          
+winny = Platypus("Winny", [0])
+steve = Platypus("Steve", [0])
+
+for seasons in range(1,11):
+    for platypus in [perry, winny, steve]:
+        platypus.lay_eggs(random.sample(range(1,4),1)[0])
+        
+perry.platypus_details()
+winny.platypus_details()
+steve.platypus_details()
+    
+undergrad_data = [Platypus("perry", [3,2,4,1,2]),
+Platypus("quacker", [100,1,3,1,2]),
+Platypus("fishface", [0,1,3,1,2,1]),
+Platypus("duckhead", [3,1,3,6,3]),
+Platypus("waddles", [3,1,2,0,8,3]),
+Platypus("professor quackington", [2,1,4,5,7]),
+Platypus("bartholomew beavertail", [0,1,3,1,0,0,2]),
+Platypus("syd", [3,1,3,1,3,5,5,2,1,3]),
+Platypus("ovide", [2,0,10,0,0,0,0,0,0]),
+Platypus("hexley", [3,1,2,3,1,0,0,1,1]),
+Platypus("supafly", [19,1,2,1,0,0,0,1])]
+
+unlikely_platyped = [platyped.name for platyped in undergrad_data if platyped.total_fecundity()/platyped.breeding_seasons() > 3]
+
+
+
